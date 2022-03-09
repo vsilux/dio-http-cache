5e00625bd891e27bb0531f71709cd5409ec508f7
Ilya Suvorov <vsilux@gmail.com>
2022-03-09 20:59:16 +0200

    updated serializer

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ilyasuvorov/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/SSD/Work/Repos/dio-http-cache/example"
+export "FLUTTER_APPLICATION_PATH=/Users/ilyasuvorov/Work/dio-http-cache/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Volumes/SSD/Work/Repos/dio-http-cache/example/lib/main.dart"
+export "FLUTTER_TARGET=/Users/ilyasuvorov/Work/dio-http-cache/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVF
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Volumes/SSD/Work/Repos/dio-http-cache/example/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/ilyasuvorov/Work/dio-http-cache/example/.dart_tool/package_config.json"
